d027f17654866b9e25efb2d443d90c951f8c1988
LAPTOP-RTVPKDPS\caila <cailanee77@gmail.com>
2023-04-27 17:02:35 -0300

    lista das teclas criada

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -2,4 +2,5 @@ function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+listaDeTeclas[0].onclick = tocaSomPom;
